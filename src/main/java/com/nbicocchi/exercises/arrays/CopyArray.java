e86ccaa1d1138843d088995151216a4e95a2f7bc
Nicola Bicocchi <nbicocchi@unimore.it>
2023-02-28 14:46:06 +0100

    update

--- src/main/java/com/nbicocchi/exercises/arrays/CopyArray.java
+++ src/main/java/com/nbicocchi/exercises/arrays/CopyArray.java
@@ -4,8 +4,6 @@ public class CopyArray {
     public static double[] copyArray(double[] src) {
         double[] dst = new double[src.length];
         System.arraycopy(src, 0, dst, 0, src.length);
-        // more compact version
-        // System.arraycopy(src, 0, dst, 0, src.length);
         return dst;
     }
 }
