3cb623dde4f86bdf088f0dbf5caa8c5aef4ce96c
ncla <ricciardincl@gmail.com>
2023-03-10 15:44:46 +0100

    Updated RationalNumber with lcm

--- src/main/java/com/nbicocchi/exercises/oop/basic/RationalNumber.java
+++ src/main/java/com/nbicocchi/exercises/oop/basic/RationalNumber.java
@@ -2,78 +2,76 @@ package com.nbicocchi.exercises.oop.basic;
 
 import java.util.Objects;
 
-/**
- * A rational number supporting addition and multiplication with other rational numbers.
- */
 public class RationalNumber {
-    final int numerator;
-    final int denominator;
 
-    /**
-     * Constructs and initializes a new rational number with the specified numerator and denominator
-     * @param numerator the numerator ot the rational
-     * @param denominator the denominator ot the rational
-     */
+    private int denominator;
+    private int numerator;
+
     public RationalNumber(int numerator, int denominator) {
-        this.numerator = numerator;
         this.denominator = denominator;
+        this.numerator = numerator;
     }
 
-    /**
-     * Returns the numerator of the rational
-     * @return the numerator
-     */
-    public int getNumerator() {
-        return numerator;
-    }
-
-    /**
-     * Returns the denominator of the rational
-     * @return the denominator
-     */
     public int getDenominator() {
         return denominator;
     }
 
-    /**
-     * Adds one rational to the current rational
-     * @param o the rational to be added
-     * @return a new rational representing the sum
-     */
-    public RationalNumber add(RationalNumber o) {
-        int n = numerator * o.denominator + o.numerator * denominator;
-        int d = denominator * o.denominator;
-        return new RationalNumber(n, d);
+    public int getNumerator() {
+        return numerator;
     }
 
-    /**
-     * Multiples one rational to the current rational
-     * @param o the rational to be multiplied
-     * @return a new rational representing the product
-     */
-    public RationalNumber multiply(RationalNumber o) {
-        int n = this.numerator * o.numerator;
-        int d = this.denominator * o.denominator;
-        return new RationalNumber(n, d);
+    public RationalNumber add(RationalNumber rationalNumberToAdd) {
+
+        int newDenominator = RationalNumber.leastCommonMultiple(this.denominator, rationalNumberToAdd.denominator);
+
+        int newNumerator = (newDenominator / this.denominator) * this.numerator;
+        newNumerator += (newDenominator / rationalNumberToAdd.denominator) * rationalNumberToAdd.numerator;
+
+        return new RationalNumber(newNumerator, newDenominator);
+
+    }
+
+    public RationalNumber multiply(RationalNumber rationalNumberToMultiply) {
+
+        return new RationalNumber(this.numerator * rationalNumberToMultiply.numerator,
+                this.denominator * rationalNumberToMultiply.denominator);
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d / %d", this.numerator, this.denominator);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
-        if (o == null || getClass() != o.getClass())
+        if (!(o instanceof RationalNumber that))
             return false;
-        RationalNumber that = (RationalNumber) o;
-        return numerator == that.numerator && denominator == that.denominator;
+        return getDenominator() == that.getDenominator() && getNumerator() == that.getNumerator();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(numerator, denominator);
+        return Objects.hash(getDenominator(), getNumerator());
     }
 
-    @Override
-    public String toString() {
-        return "Rational{" + "numerator=" + numerator + ", denominator=" + denominator + '}';
+    public static int greatestCommonDivisor(int a, int b) {
+
+        int biggerNumber = Math.max(a, b);
+        int smallestNumber = Math.min(a, b);
+
+        int rest = biggerNumber % smallestNumber;
+
+        if (rest == 0)
+            return smallestNumber;
+        else
+            return RationalNumber.greatestCommonDivisor(smallestNumber, rest);
+
+    }
+
+    public static int leastCommonMultiple(int a, int b) {
+        return Math.abs(a * b) / RationalNumber.greatestCommonDivisor(a, b);
     }
 }
--- src/test/java/com/nbicocchi/exercises/oop/basic/RationalNumberTest.java
+++ src/test/java/com/nbicocchi/exercises/oop/basic/RationalNumberTest.java
@@ -7,16 +7,52 @@ import static org.junit.jupiter.api.Assertions.*;
 class RationalNumberTest {
 
     @Test
+    void greatestCommonDivisor() {
+
+        assertEquals(7, RationalNumber.greatestCommonDivisor(7, 14));
+
+    }
+
+    @Test
+    void leastCommonMultiple() {
+        assertEquals(21, RationalNumber.leastCommonMultiple(3, 7));
+
+    }
+
+    @Test
     void add() {
-        RationalNumber r1 = new RationalNumber(2, 3);
-        RationalNumber r2 = new RationalNumber(2, 3);
-        assertEquals(new RationalNumber(12, 9), r1.add(r2));
+        RationalNumber rationalNumber1 = new RationalNumber(1, 2);
+        RationalNumber rationalNumber2 = new RationalNumber(2, 3);
+
+        RationalNumber rationalNumberAdded = rationalNumber1.add(rationalNumber2);
+
+        assertEquals(7, rationalNumberAdded.getNumerator());
+        assertEquals(6, rationalNumberAdded.getDenominator());
     }
 
     @Test
     void multiply() {
-        RationalNumber r1 = new RationalNumber(2, 3);
-        RationalNumber r2 = new RationalNumber(2, 3);
-        assertEquals(new RationalNumber(4, 9), r1.multiply(r2));
+        RationalNumber rationalNumber1 = new RationalNumber(1, 5);
+        RationalNumber rationalNumber2 = new RationalNumber(8, 1);
+
+        RationalNumber rationalNumberAdded = rationalNumber1.multiply(rationalNumber2);
+
+        assertEquals(8, rationalNumberAdded.getNumerator());
+        assertEquals(5, rationalNumberAdded.getDenominator());
+    }
+
+    @Test
+    void testToString() {
+        RationalNumber rationalNumber = new RationalNumber(5, 7);
+
+        System.out.println(rationalNumber.toString());
+    }
+
+    @Test
+    void testEquals() {
+        RationalNumber rationalNumber1 = new RationalNumber(1, 5);
+        RationalNumber rationalNumber2 = new RationalNumber(1, 5);
+
+        assertEquals(rationalNumber1, rationalNumber2);
     }
 }
