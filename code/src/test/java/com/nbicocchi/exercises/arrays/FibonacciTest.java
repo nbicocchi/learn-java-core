024bb9ac28a7d5e9f27b2db8a364534ea960a6cd
Nicola Bicocchi <nbicocchi@unimore.it>
2024-03-08 16:14:40 +0100

    update

--- code/src/test/java/com/nbicocchi/exercises/arrays/FibonacciTest.java
+++ code/src/test/java/com/nbicocchi/exercises/arrays/FibonacciTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class FibonacciTest {
     @Test
     public void fibonacci() {
-        assertArrayEquals(new long[]{0}, Fibonacci.fibonacci(1));
-        assertArrayEquals(new long[]{0, 1}, Fibonacci.fibonacci(2));
-        assertArrayEquals(new long[]{0, 1, 1, 2, 3}, Fibonacci.fibonacci(5));
+        assertArrayEquals(new long[]{1}, Fibonacci.fibonacci(1));
+        assertArrayEquals(new long[]{1, 1}, Fibonacci.fibonacci(2));
+        assertArrayEquals(new long[]{1, 1, 2, 3, 5}, Fibonacci.fibonacci(5));
     }
 }
