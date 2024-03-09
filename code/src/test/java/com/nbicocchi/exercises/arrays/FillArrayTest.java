d0ead1c0d3ad6e840866a8c9ac6ed39f6da63ee8
Nicola Bicocchi <nbicocchi@unimore.it>
2024-03-09 18:59:29 +0100

    update

--- code/src/test/java/com/nbicocchi/exercises/arrays/FillArrayTest.java
+++ code/src/test/java/com/nbicocchi/exercises/arrays/FillArrayTest.java
@@ -10,5 +10,7 @@ class FillArrayTest {
     void fillArray() {
         assertArrayEquals(new double[]{1, 1, 1}, FillArray.fillArray(3, 1.0, false));
         assertArrayEquals(new double[]{5, 5, 5, 5}, FillArray.fillArray(4, 5.0, false));
+        assertArrayEquals(new double[]{10, 10, 10, 10, 10, 10}, FillArray.fillArray(6, 10.0, true), 0.5);
+        assertArrayEquals(new double[]{1, 1, 1, 1, 1, 1}, FillArray.fillArray(6, 1.0, true), 0.05);
     }
 }
