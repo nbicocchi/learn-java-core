d8169c6b5f14d6f5ec3ebe1fdc439ae0758a2baf
Nicola Bicocchi <nbicocchi@unimore.it>
2024-03-09 23:40:40 +0100

    update

--- code/exercises/src/test/java/com/nbicocchi/exercises/arrays/FizzBuzzTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/arrays/FizzBuzzTest.java
@@ -10,5 +10,8 @@ class FizzBuzzTest {
         assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz"}, FizzBuzz.fizzBuzz(1, 6));
         assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7"}, FizzBuzz.fizzBuzz(1, 8));
         assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"}, FizzBuzz.fizzBuzz(1, 11));
+        assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11" , "Fizz" , "13", "14" , "FizzBuzz"}, FizzBuzz.fizzBuzz(1, 16));
+        assertArrayEquals(new String[]{"Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"}, FizzBuzz.fizzBuzz(3, 11));
+        assertArrayEquals(new String[]{"Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11" , "Fizz" , "13", "14" , "FizzBuzz"}, FizzBuzz.fizzBuzz(5, 16));
     }
 }
