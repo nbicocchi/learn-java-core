3793d924334d3b52d22b9f2a7e38a6ccb6acdafb
Marin Cervinschi <129498559+MarinCervinschi@users.noreply.github.com>
2024-03-15 18:57:23 +0100

    Update BankAccountEasyTest.java

--- code/exercises/src/test/java/com/nbicocchi/exercises/oop/bankaccount/BankAccountEasyTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/oop/bankaccount/BankAccountEasyTest.java
@@ -9,7 +9,7 @@ class BankAccountEasyTest {
     @Test
     void withdraw() {
         BankAccountEasy ba = new BankAccountEasy("IT0000012345", 1000);
-        assertEquals(1000, ba.withdraw(2000));
+        ba.withdraw(2000);
         assertEquals(0, ba.getBalance());
     }
 
@@ -19,7 +19,7 @@ class BankAccountEasyTest {
         BankAccountEasy ba2 = new BankAccountEasy("DE0000012345", 1000);
         BankAccountEasy ba3 = new BankAccountEasy("IT0000012345", 1000);
         assertThrows(IllegalArgumentException.class, () -> ba1.transfer(ba2, 1000));
-        assertEquals(1000, ba1.transfer(ba3,2000));
+        ba1.transfer(ba3, 2000);
         assertEquals(0, ba1.getBalance());
         assertEquals(2000, ba3.getBalance());
     }
