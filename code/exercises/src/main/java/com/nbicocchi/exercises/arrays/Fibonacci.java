ef1f925424cc361358825a56bc49cdd2fc258a48
Nicola Bicocchi <nbicocchi@unimore.it>
2024-03-15 08:56:36 +0100

    update

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/Fibonacci.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/Fibonacci.java
@@ -4,12 +4,12 @@ public class Fibonacci {
     public static long[] fibonacci(int n) {
         long[] fibonacci;
         if (n == 1) {
-            fibonacci = new long[]{1};
+            fibonacci = new long[]{0};
         } else if (n == 2) {
-            fibonacci = new long[]{1, 1};
+            fibonacci = new long[]{0, 1};
         } else {
             fibonacci = new long[n];
-            fibonacci[0] = 1;
+            fibonacci[0] = 0;
             fibonacci[1] = 1;
             for (int i = 2; i < n; i++) {
                 fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
