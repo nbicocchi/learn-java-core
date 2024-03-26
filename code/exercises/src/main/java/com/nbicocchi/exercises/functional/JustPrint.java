41ddf12552a0a5102783b2be99f03357f0b250ef
Nicola Bicocchi <nbicocchi@unimore.it>
2024-03-26 22:19:10 +0100

    update

--- code/exercises/src/main/java/com/nbicocchi/exercises/functional/JustPrint.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/functional/JustPrint.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public class JustPrint {
-    public static <T> void justPrintList(List<T> strings) {
-        strings.forEach(s -> System.out.println("Hello " + s + "!"));
+    public static <T> void justPrintList(List<T> list) {
+        list.forEach(s -> System.out.println("Hello " + s + "!"));
     }
 
     public static <K,V> void justPrintMap(Map<K, V> map) {
--- code/exercises/src/main/java/com/nbicocchi/exercises/functional/JustPrintParametrized.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/functional/JustPrintParametrized.java
@@ -6,8 +6,8 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class JustPrintParametrized {
-    public static <T> void justPrintList(List<T> strings, Consumer<? super T> consumer) {
-        strings.forEach(consumer);
+    public static <T> void justPrintList(List<T> list, Consumer<? super T> consumer) {
+        list.forEach(consumer);
     }
 
     public static <K,V> void justPrintMap(Map<K, V> map, BiConsumer<? super K, ? super V> biConsumer) {
