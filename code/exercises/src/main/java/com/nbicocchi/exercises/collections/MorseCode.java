9c422f0660da236347726f975904ff9cecea0617
Marin Cervinschi <129498559+MarinCervinschi@users.noreply.github.com>
2024-03-29 17:40:08 +0100

    Update MorseCode.java

--- code/exercises/src/main/java/com/nbicocchi/exercises/collections/MorseCode.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/collections/MorseCode.java
@@ -6,54 +6,49 @@ import java.util.Set;
 
 public class MorseCode {
 
-    public static Map<Character, String> conversionMap() {
-        HashMap<Character, String> conversionMap = new HashMap<>();
-        conversionMap.put('a', "._");
-        conversionMap.put('b', "_...");
-        conversionMap.put('c', "_._.");
-        conversionMap.put('d', "_..");
-        conversionMap.put('e', ".");
-        conversionMap.put('f', ".._.");
-        conversionMap.put('g', "__.");
-        conversionMap.put('h', "....");
-        conversionMap.put('i', "..");
-        conversionMap.put('j', ".___");
-        conversionMap.put('k', "_._");
-        conversionMap.put('l', "._..");
-        conversionMap.put('m', "__");
-        conversionMap.put('n', "_.");
-        conversionMap.put('o', "___");
-        conversionMap.put('p', ".__.");
-        conversionMap.put('q', "__._");
-        conversionMap.put('r', "._.");
-        conversionMap.put('s', "...");
-        conversionMap.put('t', "_");
-        conversionMap.put('u', ".._");
-        conversionMap.put('v', "..._");
-        conversionMap.put('w', ".__");
-        conversionMap.put('x', "_.._");
-        conversionMap.put('y', "_.__");
-        conversionMap.put('z', "__..");
-        return conversionMap;
-    }
-
-    public static void checkString(Set<Character> allowedChars, String string) {
-        for (char c : string.toLowerCase().toCharArray()) {
-            if (!allowedChars.contains(c)) {
-                throw new IllegalArgumentException("String contains unsupported char");
-            }
-        }
-    }
-
     public static String morseCode(String string) {
-        Map<Character, String> conversionMap = conversionMap();
-        checkString(conversionMap.keySet(), string);
-
-        StringBuilder builder = new StringBuilder();
-        for (char c : string.toLowerCase().toCharArray()) {
-            builder.append(conversionMap.get(c)).append(" ");
+        if (string.isEmpty()) {
+            return "";
         }
+        if (!string.matches("^[a-zA-Z]*$")) {
+            throw new IllegalArgumentException("String must contains only alphabet letters");
+        }
+
+        Map<String, String> morse = Map.ofEntries(
+            Map.entry("a", "._"),
+            Map.entry("b", "_..."),
+            Map.entry("c", "_._."),
+            Map.entry("d", "_.."),
+            Map.entry("e", "."),
+            Map.entry("f", ".._."),
+            Map.entry("g", "__."),
+            Map.entry("h", "...."),
+            Map.entry("i", ".."),
+            Map.entry("j", ".___"),
+            Map.entry("k", "_._"),
+            Map.entry("l", "._.."),
+            Map.entry("m", "__"),
+            Map.entry("n", "_."),
+            Map.entry("o", "___"),
+            Map.entry("p", ".__."),
+            Map.entry("q", "__._"),
+            Map.entry("r", "._."),
+            Map.entry("s", "..."),
+            Map.entry("t", "_"),
+            Map.entry("u", ".._"),
+            Map.entry("v", "..._"),
+            Map.entry("w", ".__"),
+            Map.entry("x", "_.._"),
+            Map.entry("y", "_.__"),
+            Map.entry("z", "__..")
+        );
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : string.toLowerCase().toCharArray()) {
+            sb.append(morse.get(String.valueOf(c))).append(" ");
+        }
+
         // trim() eventually removes the last space
-        return builder.toString().trim();
+        return sb.toString().trim();
     }
 }
