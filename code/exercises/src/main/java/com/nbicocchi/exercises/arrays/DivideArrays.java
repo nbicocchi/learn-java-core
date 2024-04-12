632627d12bd2ad1e49c71bb48340b6d0943691ee
gabbonj <61253079+gabbonj@users.noreply.github.com>
2024-04-12 19:50:12 +0200

    Coherent variable naming between README.md and solutions in DivideArray and DivideArrays

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/DivideArrays.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/DivideArrays.java
@@ -1,12 +1,12 @@
 package com.nbicocchi.exercises.arrays;
 
 public class DivideArrays {
-    public static double[] divideArrays(double[] a, double[] b) {
-        if (a.length != b.length)
+    public static double[] divideArrays(double[] v1, double[] v2) {
+        if (v1.length != v2.length)
             return new double[]{};
-        double[] out = new double[a.length];
-        for (int i = 0; i < a.length; i++) {
-            out[i] = a[i] / b[i];
+        double[] out = new double[v1.length];
+        for (int i = 0; i < v1.length; i++) {
+            out[i] = v1[i] / v2[i];
         }
         return out;
     }
