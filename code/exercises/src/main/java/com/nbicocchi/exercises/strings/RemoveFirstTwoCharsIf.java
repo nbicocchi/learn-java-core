b09b5de7ff8cebc850c58f140052ab4ccf4e3ed8
gabbonj <61253079+gabbonj@users.noreply.github.com>
2024-04-12 18:50:22 +0200

    Revert "more compact solution for RemoveFirstTwoCharsIf"
    This reverts commit 1260bb02d8c5095a1a685547313242a0afd36823.

--- code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoCharsIf.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoCharsIf.java
@@ -3,9 +3,24 @@ package com.nbicocchi.exercises.strings;
 public class RemoveFirstTwoCharsIf {
     public static String removeFirstTwoCharsIf(String string) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == 'H' || string.charAt(i) == 'e' || i > 1) {
-                sb.append(string.charAt(i));
+        switch (string.length()) {
+            case 0 -> {}
+            case 1 -> {
+                if (string.charAt(0) == 'H')
+                    sb.append("H");
+            }
+            case 2 -> {
+                if (string.charAt(0) == 'H')
+                    sb.append("H");
+                if (string.charAt(1) == 'e')
+                    sb.append("e");
+            }
+            default -> {
+                if (string.charAt(0) == 'H')
+                    sb.append("H");
+                if (string.charAt(1) == 'e')
+                    sb.append("e");
+                sb.append(string.substring(2));
             }
         }
         return sb.toString();
