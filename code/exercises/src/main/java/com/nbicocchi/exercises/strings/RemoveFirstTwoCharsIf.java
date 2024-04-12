1260bb02d8c5095a1a685547313242a0afd36823
gabbonj <61253079+gabbonj@users.noreply.github.com>
2024-04-12 18:44:30 +0200

    more compact solution for RemoveFirstTwoCharsIf

--- code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoCharsIf.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoCharsIf.java
@@ -3,24 +3,9 @@ package com.nbicocchi.exercises.strings;
 public class RemoveFirstTwoCharsIf {
     public static String removeFirstTwoCharsIf(String string) {
         StringBuilder sb = new StringBuilder();
-        switch (string.length()) {
-            case 0 -> {}
-            case 1 -> {
-                if (string.charAt(0) == 'H')
-                    sb.append("H");
-            }
-            case 2 -> {
-                if (string.charAt(0) == 'H')
-                    sb.append("H");
-                if (string.charAt(1) == 'e')
-                    sb.append("e");
-            }
-            default -> {
-                if (string.charAt(0) == 'H')
-                    sb.append("H");
-                if (string.charAt(1) == 'e')
-                    sb.append("e");
-                sb.append(string.substring(2));
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == 'H' || string.charAt(i) == 'e' || i > 1) {
+                sb.append(string.charAt(i));
             }
         }
         return sb.toString();
