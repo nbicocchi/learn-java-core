4d13fe7a26ead50929cd88d77fafd4db95abb58f
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-04-22 13:51:32 +0200

    Update CheckAnagrams.java
    Simplified code

--- code/exercises/src/main/java/com/nbicocchi/exercises/collections/CheckAnagrams.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/collections/CheckAnagrams.java
@@ -24,8 +24,7 @@ public class CheckAnagrams {
     private static Map<Character, Integer> stringToAppearencesMap(String string) {
         Map<Character, Integer> appearances = new HashMap<>();
         for (Character c : string.toLowerCase().toCharArray()) {
-            int numOfAppearances = appearances.getOrDefault(c, 0);
-            appearances.put(c, numOfAppearances + 1);
+            appearances.put(c, appearances.getOrDefault(c, 0) + 1);
         }
         return appearances;
     }
