305ba91918bc7c6e6a1822e32d7f2704a9d8d291
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-04-24 12:58:53 +0200

    Update RemoveFirstTwoChars.java
    Using .delete(0, 2) method

--- code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoChars.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoChars.java
@@ -2,14 +2,6 @@ package com.nbicocchi.exercises.strings;
 
 public class RemoveFirstTwoChars {
     public static String removeFirstTwoChars(String s) {
-        if (s.length() < 3) {
-            return "";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 2; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-        }
-        return sb.toString();
+        return new StringBuilder(s).delete(0, 2).toString();
     }
 }
