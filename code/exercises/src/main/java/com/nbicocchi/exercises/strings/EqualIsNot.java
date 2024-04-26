9885315438b3ee38b8b2323095277e8f84fb895a
Nicola Bicocchi <nbicocchi@unimore.it>
2024-04-26 12:21:49 +0200

    update

--- code/exercises/src/main/java/com/nbicocchi/exercises/strings/EqualIsNot.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/strings/EqualIsNot.java
@@ -3,23 +3,12 @@ package com.nbicocchi.exercises.strings;
 public class EqualIsNot {
 
     /**
-     * Checks if a string is empty ("") or null.
-     *
-     * @param s the string to be verified
-     * @return true if the string is null or empty
-     */
-    public static boolean isEmpty(String s) {
-        return s == null || s.isEmpty();
-    }
-
-
-    /**
      * Returns true if the number of occurrences of "is" equals the number of occurrences of "not"
      * @param string the string to be processed
      * @return true if the number of occurrences of "is" equals the number of occurrences of "not"
      */
     public static boolean equalIsNot(String string) {
-        if (isEmpty(string)) {
+        if (string == null || string.isEmpty()) {
             return true;
         }
 
