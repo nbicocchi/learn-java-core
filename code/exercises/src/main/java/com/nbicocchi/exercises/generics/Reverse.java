2eca1e3065df834527f920ba111ad5458a7669fb
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-05-03 15:02:47 +0200

    Update Reverse.java
    I think that this way is cleaner and more readable

--- code/exercises/src/main/java/com/nbicocchi/exercises/generics/Reverse.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/generics/Reverse.java
@@ -7,8 +7,9 @@ public class Reverse {
         int size = list.size();
         for (int i = 0; i < Math.floor((double) size / 2); i++) {
             T tmp = list.get(i);
-            list.set(i, list.get(size - i - 1));
-            list.set(size - i - 1, tmp);
+            int specularIndex = size - i - 1;
+            list.set(i, list.get(specularIndex));
+            list.set(specularIndex, tmp);
         }
     }
 }
