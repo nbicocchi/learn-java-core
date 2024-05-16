d74703436a0a5289f20ccef843a85be62c33f12d
Luca <129498542+LucaFerraro03@users.noreply.github.com>
2024-05-16 15:37:19 +0200

    Update RomanToDecimal.java

--- code/exercises/src/main/java/com/nbicocchi/exercises/collections/RomanToDecimal.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/collections/RomanToDecimal.java
@@ -15,10 +15,18 @@ public class RomanToDecimal {
         );
     }
     public static int romanToDecimal(String s) {
+
+        if( s.length() < 1 || s.length() > 15)
+            throw new IllegalArgumentException();
+        
         Map<Character, Integer> conversionMap = conversionMap();
 
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
+            
+            if(!conversionMap.containsKey(s.charAt(i)))
+                throw new IllegalArgumentException();
+            
             if ((i < s.length() - 1) && conversionMap.get(s.charAt(i)) < conversionMap.get(s.charAt(i + 1))) {
                 result -= conversionMap.get(s.charAt(i));
             } else {
