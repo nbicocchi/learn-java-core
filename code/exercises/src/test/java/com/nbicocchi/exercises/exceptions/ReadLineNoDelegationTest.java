73ce3091d36ea5f0e8d38ffa5805e88068fc9271
Mattia Gualtieri <mattiagua03@gmail.com>
2024-05-16 15:02:33 +0200

    Update of 3 exercises within exceptions

--- code/exercises/src/test/java/com/nbicocchi/exercises/exceptions/ReadLineNoDelegationTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/exceptions/ReadLineNoDelegationTest.java
@@ -2,11 +2,13 @@ package com.nbicocchi.exercises.exceptions;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ReadLineNoDelegationTest {
     @Test
     void noDelegation() {
         assertNull(ReadLineNoDelegation.readLineNoDelegation("/tmp/missing"));
+        assertEquals("Hello!",ReadLineNoDelegation.readLineNoDelegation("src/main/resources/Hello"));
     }
 }
--- code/exercises/src/test/java/com/nbicocchi/exercises/exceptions/ReadLinePartialDelegationTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/exceptions/ReadLinePartialDelegationTest.java
@@ -2,11 +2,13 @@ package com.nbicocchi.exercises.exceptions;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReadLinePartialDelegationTest {
     @Test
     void readLinePartialDelegationTest() {
         assertThrows(RuntimeException.class, () -> ReadLinePartialDelegation.readLinePartialDelegation("/tmp/missing"));
+        assertEquals("Hello!",ReadLinePartialDelegation.readLinePartialDelegation("src/main/resources/Hello"));
     }
 }
