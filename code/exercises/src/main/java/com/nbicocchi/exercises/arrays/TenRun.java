c28d59eaf2a3231233740c713d6b3041498a0de4
vincenzoparente <129498503+vincenzoparente@users.noreply.github.com>
2024-05-23 09:12:16 +0200

    Update TenRun.java
    find a shorter solution

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/TenRun.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/TenRun.java
@@ -1,13 +1,10 @@
 package com.nbicocchi.exercises.arrays;
 
 public class TenRun {
-    public static int[] tenRun(int[] v) {
-        int multiple = 0;
-        for (int i = 0; i < v.length; i++) {
-            if (v[i] != 0 && v[i] % 10 == 0) {
-                multiple = v[i];
-            } else if (multiple != 0) {
-                v[i] = multiple;
+    public static int[] tenRun(int[] v){
+        for (int i = 0; i < v.length-1; i++){
+            if(v[i]%10==0  && v[i+1]%10!=0){
+               v[i+1]=v[i];
             }
         }
         return v;
