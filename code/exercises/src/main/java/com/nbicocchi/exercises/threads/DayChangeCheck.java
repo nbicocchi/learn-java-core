5a07daef222e9660953e3bf318b7087186355fa6
M61ss <mattia.mas2003@gmail.com>
2024-06-14 18:51:21 +0200

    Update DayChangeCheck.java

--- code/exercises/src/main/java/com/nbicocchi/exercises/threads/DayChangeCheck.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/threads/DayChangeCheck.java
@@ -4,13 +4,16 @@ import java.time.LocalDate;
 import java.util.concurrent.TimeUnit;
 
 public class DayChangeCheck extends Thread {
+    public DayChangeCheck(String name) {
+        super(name);
+    }
 
     @Override
     public void run() {
         System.out.println(getName() + " started");
+        LocalDate lastCheck = LocalDate.now();
         while (!interrupted()) {
-            LocalDate lastCheck = LocalDate.now();
-
+            System.out.println("Today is " + lastCheck);
             try {
                 Thread.sleep(TimeUnit.MINUTES.toMillis(15));
             } catch (InterruptedException ignored) {
@@ -18,15 +21,16 @@ public class DayChangeCheck extends Thread {
             }
 
             LocalDate now = LocalDate.now();
-            if (now.getDayOfMonth() != lastCheck.getDayOfMonth()) {
+            if (now.isAfter(lastCheck)) {
                 System.out.println(getName() + " the day of month changed");
+                lastCheck = now;
             }
         }
         System.out.println(getName() + " terminated");
     }
 
     public static void main(String[] args) {
-        DayChangeCheck t1 = new DayChangeCheck();
+        DayChangeCheck t1 = new DayChangeCheck("DayChangeCheck");
         t1.start();
     }
 }
