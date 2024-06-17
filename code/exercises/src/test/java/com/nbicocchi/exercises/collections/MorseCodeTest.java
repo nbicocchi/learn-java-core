9838e18a1ec4b4f886107c38bad65a04bedb6c34
mbernabei <mattia.bernabei2@gmail.com>
2024-06-17 18:01:41 +0200

    Small Update on an Exercise
    Added numbers to morse code map and changed the test properly.
    Removed checkString method

--- code/exercises/src/test/java/com/nbicocchi/exercises/collections/MorseCodeTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/collections/MorseCodeTest.java
@@ -10,7 +10,8 @@ class MorseCodeTest {
     void morseCode() {
         assertEquals(".... . ._.. ._.. ___", MorseCode.morseCode("hello"));
         assertEquals(".__ ___ ._. ._.. _..", MorseCode.morseCode("world"));
+        assertEquals(".__ _____ ._. ._.. _..", MorseCode.morseCode("w0rld"));
         assertEquals("", MorseCode.morseCode(""));
-        assertThrows(IllegalArgumentException.class, () -> MorseCode.morseCode("w0rld"));
+        assertThrows(IllegalArgumentException.class, () -> MorseCode.morseCode("w$rld"));
     }
 }
