42d3b7d7ea586281511efaf52d5ebf30016bc0f2
Nicola Bicocchi <nbicocchi@unimore.it>
2024-06-17 11:37:19 +0200

    update

--- code/exercises/src/main/java/com/nbicocchi/exercises/collections/LookAndSay.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/collections/LookAndSay.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LookAndSay {
-    public static List<Long> lookAndSay(long n){
-        if(n < 1) {
+    public static List<Long> lookAndSay(long n) {
+        if (n < 1) {
             return new ArrayList<>();
         }
 
         List<Long> sequence = new ArrayList<>();
         sequence.add(1L);
-        for(int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             String previous = sequence.get(i - 1).toString();
             StringBuilder next = new StringBuilder();
             int count = 1;
-            for(int j = 1; j < previous.length(); j++) {
-                if(previous.charAt(j) == previous.charAt(j - 1)) {
+            for (int j = 1; j < previous.length(); j++) {
+                if (previous.charAt(j) == previous.charAt(j - 1)) {
                     count++;
                 } else {
                     next.append(count).append(previous.charAt(j - 1));
