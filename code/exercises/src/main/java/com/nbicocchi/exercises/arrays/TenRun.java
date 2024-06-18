f5713be2c4949c2e938e575d8f765853897e489b
Lin <linzhouandrea2003@gmail.com>
2024-06-18 16:06:45 +0200

    fix TenRun: 0 is a multiple of 10

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/TenRun.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/TenRun.java
@@ -2,11 +2,11 @@ package com.nbicocchi.exercises.arrays;
 
 public class TenRun {
     public static int[] tenRun(int[] v) {
-        int multiple = 0;
+        int multiple = -1;
         for (int i = 0; i < v.length; i++) {
-            if (v[i] != 0 && v[i] % 10 == 0) {
+            if (v[i] % 10 == 0) {
                 multiple = v[i];
-            } else if (multiple != 0) {
+            } else if (multiple != -1) {
                 v[i] = multiple;
             }
         }
--- code/exercises/src/test/java/com/nbicocchi/exercises/arrays/TenRunTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/arrays/TenRunTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class TenRunTest {
     @Test
     void tenRun() {
+        assertArrayEquals(new int[]{0, 0, 10, 10}, TenRun.tenRun(new int[]{0, 2, 10, 14}));
         assertArrayEquals(new int[]{2, 10, 10, 10, 20, 20}, TenRun.tenRun(new int[]{2, 10, 3, 4, 20, 5}));
         assertArrayEquals(new int[]{10, 10, 20, 20}, TenRun.tenRun(new int[]{10, 1, 20, 2}));
         assertArrayEquals(new int[]{10, 10, 10, 20}, TenRun.tenRun(new int[]{10, 1, 9, 20}));
