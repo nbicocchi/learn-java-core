214b4476faa7e66261e9e4d5ad963c52d7e98337
Lin <linzhouandrea2003@gmail.com>
2024-07-13 02:34:03 +0200

    Fix-RemoveFirstTwoCharsIf: maybe without the switch and duplicated code is more clear

--- code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoCharsIf.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstTwoCharsIf.java
@@ -3,26 +3,15 @@ package com.nbicocchi.exercises.strings;
 public class RemoveFirstTwoCharsIf {
     public static String removeFirstTwoCharsIfCharAt(String string) {
         StringBuilder sb = new StringBuilder();
-        switch (string.length()) {
-            case 0 -> {
-            }
-            case 1 -> {
-                if (string.charAt(0) == 'H')
-                    sb.append("H");
-            }
-            case 2 -> {
-                if (string.charAt(0) == 'H')
-                    sb.append("H");
-                if (string.charAt(1) == 'e')
-                    sb.append("e");
-            }
-            default -> {
-                if (string.charAt(0) == 'H')
-                    sb.append("H");
-                if (string.charAt(1) == 'e')
-                    sb.append("e");
-                sb.append(string.substring(2));
-            }
+
+        if (!string.isEmpty() && string.charAt(0) == 'H') {
+            sb.append('H');
+        }
+        if (string.length() >= 2 && string.charAt(1) == 'e') {
+            sb.append('e');
+        }
+        if (string.length() >= 3) {
+            sb.append(string.substring(2));
         }
         return sb.toString();
     }
