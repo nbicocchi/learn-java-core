d6914979c99c54536c5b87b3e5e42eb8b3beb561
Marra-Federico <marra.federico23@gmail.com>
2024-07-14 11:31:19 +0200

    Alternative solution of the Word exercise in functional using the method given by the text

--- code/exercises/src/main/java/com/nbicocchi/exercises/functional/Words.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/functional/Words.java
@@ -7,17 +7,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Words {
-    public static long howManyLines(String filename, String subString) throws IOException {
-        List<String> book = getLines(filename);
-        return book.stream().filter(line -> line.contains(subString)).count();
+
+    public static List<String> lineToWords(String line) {
+        return Arrays.asList(line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+"));
     }
 
     public static List<String> getLines(String filename) throws IOException {
         return Files.readAllLines(Path.of(filename));
     }
 
+    public static long howManyLines(String filename, String subString) throws IOException {
+        List<String> book = getLines(filename);
+        return book.stream().filter(line -> line.contains(subString)).count();
+    }
+
+
+
     public static long howManyTimes(String filename, String word) throws IOException {
         List<String> book = getLines(filename);
-        return book.stream().flatMap(line -> Arrays.stream(line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+"))).filter(w -> w.equals(word)).count();
+        return book.stream().flatMap(line -> lineToWords(line).stream().filter(w -> w.compareTo(word) == 0)).count();
     }
 }
