123956ac64bd71078c0be703fb7aac522f276fe6
Hamza <81712650+HamzaTouijer@users.noreply.github.com>
2024-08-10 16:53:24 +0200

    Update BankAccountTest.java
    setting up a bank account before each test to avoid repetition.

--- code/exercises/src/test/java/com/nbicocchi/exercises/oop/basic/BankAccountTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/oop/basic/BankAccountTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
-
+    
+   BankAccount bankAccount;
+    
+    @BeforeEach
+    void setUp(){
+        bankAccount = new BankAccount(1000);
+    }
+    
     @Test
     void getBalance() {
-        BankAccount bankAccount = new BankAccount(1000);
         assertEquals(1000, bankAccount.getBalance());
         BankAccount emptyBankAccount = new BankAccount();
         assertEquals(0, emptyBankAccount.getBalance());
@@ -16,14 +22,12 @@ class BankAccountTest {
 
     @Test
     void deposit() {
-        BankAccount bankAccount = new BankAccount(1000);
         bankAccount.deposit(1000);
         assertEquals(2000, bankAccount.getBalance());
     }
 
     @Test
     void withdraw() {
-        BankAccount bankAccount = new BankAccount(1000);
         bankAccount.withdraw(1000);
         assertEquals(0, bankAccount.getBalance());
     }
