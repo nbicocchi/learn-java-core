5990fd88091dd5df71588b6bdae4ec8f817f6294
Nicola Bicocchi <nbicocchi@unimore.it>
2024-10-21 15:16:36 +0200

    update

--- code/exercises/src/test/java/com/nbicocchi/exercises/arrays/DeepCopyTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/arrays/DeepCopyTest.java
@@ -8,17 +8,17 @@ class DeepCopyTest {
 
     @Test
     void deepCopy() {
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}};
-        int[][] deepCopy = DeepCopy.deepCopy(matrix);
-        assertTrue(deepCopy[0] != matrix[0]);
-        assertTrue(deepCopy[1] != matrix[1]);
-
-        int[][] matrix1 = {{4, 5, 3}, {2, 1, 6}, {7, 0}, {1, 5, 2, 7}};
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}};
         int[][] deepCopy1 = DeepCopy.deepCopy(matrix1);
-        assertTrue(deepCopy1[0] != matrix1[0]);
-        assertTrue(deepCopy1[1] != matrix1[1]);
-        assertTrue(deepCopy1[2] != matrix1[2]);
-        assertTrue(deepCopy1[3] != matrix1[3]);
+        assertNotSame(deepCopy1[0], matrix1[0]);
+        assertNotSame(deepCopy1[1], matrix1[1]);
+
+        int[][] matrix2 = {{4, 5, 3}, {2, 1, 6}, {7, 0}, {1, 5, 2, 7}};
+        int[][] deepCopy2 = DeepCopy.deepCopy(matrix2);
+        assertNotSame(deepCopy2[0], matrix2[0]);
+        assertNotSame(deepCopy2[1], matrix2[1]);
+        assertNotSame(deepCopy2[2], matrix2[2]);
+        assertNotSame(deepCopy2[3], matrix2[3]);
 
     }
 }
