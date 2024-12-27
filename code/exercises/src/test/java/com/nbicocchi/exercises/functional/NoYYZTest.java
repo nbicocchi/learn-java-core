0c89cee9e99696e197b3c21886ba89b4182a1101
emanuela ugolotti <emanuela.ugolotti@gmail.com>
2024-12-27 15:07:08 +0100

    update NoYYZ

--- code/exercises/src/test/java/com/nbicocchi/exercises/functional/NoYYZTest.java
+++ code/exercises/src/test/java/com/nbicocchi/exercises/functional/NoYYZTest.java
@@ -12,5 +12,8 @@ class NoYYZTest {
         assertEquals(List.of("ay", "by", "cy"), NoYYZ.noYYZ(List.of("a", "b", "c")));
         assertEquals(List.of("ay", "by"), NoYYZ.noYYZ(List.of("a", "b", "cy")));
         assertEquals(List.of("xxy", "yay", "zzy"), NoYYZ.noYYZ(List.of("xx", "ya", "zz")));
+        assertEquals(List.of("zccy", "yay"), NoYYZ.noYYZ(List.of("zcc", "ya", "zby")));
+        assertEquals(List.of("xxy", "yay", "zy", "by"), NoYYZ.noYYZ(List.of("xx", "zaby", "ya", "z", "b")));
+        assertEquals(List.of("ay", "by", "y"), NoYYZ.noYYZ(List.of("a", "b", "zyyc", "eyybb", "")));
     }
 }
