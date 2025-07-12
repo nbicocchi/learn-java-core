d4d689b4c30c786db5885bbf503f2a902f9e6c75
Alexandro_Nistiriuc <nistiriucalexandro@gmail.com>
2025-07-12 12:55:14 +0200

    Proposta esercizio MinMax dei generics e volevo provare a fare una pull request da IntelliJ :)

--- code/exercises/src/main/java/com/nbicocchi/exercises/generics/MinMax.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/generics/MinMax.java
@@ -1,26 +1,16 @@
 package com.nbicocchi.exercises.generics;
 
-import java.util.Comparator;
-import java.util.List;
-
 public class MinMax {
     public static <T> T max(List<T> list, Comparator<? super T> cmp) {
-        T candidate = list.get(0);
-        for (T next : list) {
-            if (cmp.compare(candidate, next) < 0) {
-                candidate = next;
-            }
-        }
-        return candidate;
+        List<T> tmp = new ArrayList<>(list);
+        tmp.sort(cmp);
+        return tmp.get(list.size() - 1);
     }
 
-    public static <T> T min(List<T> list, Comparator<? super T> cmp) {
-        T candidate = list.get(0);
-        for (T next : list) {
-            if (cmp.compare(candidate, next) > 0) {
-                candidate = next;
-            }
-        }
-        return candidate;
+    public static <T> T min(List<T> list, Comparator<? super T> cmp){
+        List<T> tmp = new ArrayList<>(list);
+        tmp.sort(cmp);
+        return tmp.get(0);
     }
 }
+
