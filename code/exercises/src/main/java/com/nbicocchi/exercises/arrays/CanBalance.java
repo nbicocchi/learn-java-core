369728c707bac6318a9c5cecd6c0e8ae7ec0a037
Daniel Aniculaesei <145033708+danianiculaesei@users.noreply.github.com>
2025-09-22 14:49:23 +0200

    canBalance simplified sum calculation and 1 method
    Instead of doing 2 methods and writing more code, I find this solution easier to understand and to solve this problem. 
    I do the total sum of the vector, in a for cicle I calculate the left sum and then with subtraction i find the rigth sum, if them are equal means that the array can split.

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/CanBalance.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/CanBalance.java
@@ -1,27 +1,21 @@
 package com.nbicocchi.exercises.arrays;
 
 public class CanBalance {
-    public static int sum(int[] v, int begin, int end) {
-        int sum = 0;
-        for (int i = begin; i < end; i++) {
-            sum += v[i];
-        }
-        return sum;
-    }
-
     public static boolean canBalance(int[] v) {
-        if (v.length < 2) {
-            return false;
+        int totalSum = 0;
+        for (int n : v) {
+            totalSum += n;
         }
-        int sumBegin = sum(v, 0, 1);
-        int sumEnd = sum(v, 1, v.length);
-        for (int i = 1; i < v.length; i++) {
-            if (sumBegin == sumEnd) {
+
+        int leftSum = 0;
+        for (int i = 0; i < v.length - 1; i++) {
+            leftSum += v[i];
+            int rightSum = totalSum - leftSum;
+            if (leftSum == rightSum) {
                 return true;
             }
-            sumBegin += v[i];
-            sumEnd -= v[i];
         }
+
         return false;
     }
 }
