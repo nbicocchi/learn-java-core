beaf10234d422f3a2fd1f3d1b56a850f692c51bf
Daniel Aniculaesei <145033708+danianiculaesei@users.noreply.github.com>
2025-09-24 01:22:07 +0200

    removeFirstLast method simplified
    I simplified the method not using the two var that was implemented in your soulution, maybe this would use less memory (also if it is not  a big difference :) )

--- code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstLast.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/strings/RemoveFirstLast.java
@@ -1,14 +1,11 @@
 package com.nbicocchi.exercises.strings;
 
 public class RemoveFirstLast {
-    public static String removeFirstLast(String s) {
-        if (s.length() < 2) {
+    public static String removeFirstLast(String s){
+        if(s.length() < 2){
             return s;
         }
-
-        char first = s.charAt(0);
-        char last = s.charAt(s.length() - 1);
-        if (first == last) {
+        if(s.charAt(0) == s.charAt(s.length() - 1)){
             return s.substring(1, s.length() - 1);
         }
         return s;
